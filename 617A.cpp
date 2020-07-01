f4b5608058d001faba1848d1eb997bea001bc66a
DDR13GIT <ddroy13@gmail.com>
2020-07-01 18:54:51 +0600

    test commit

---
+++ 617A.cpp
@@ -0,0 +1,18 @@
+/******************************************
+*  Author : Debopriya Deb Roy  
+*  Created On : Wed Jul 01 2020
+*  File : c++ file
+*******************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+int i,j,k,l,m,n;
+cin >> n;
+if(n%5==0) 
+    cout<< n/5 << endl;
+else 
+    cout<< (n/5)+1 << endl;
+    
+    return 0;
+}
